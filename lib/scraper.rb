41ae0a11229aa79a1abad6d15755e7c2b3ffb33d
Martin Achtermann <mdacht777@gmail.com>
2020-07-06 02:59:59 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -23,7 +23,7 @@ class Scraper
   def get_courses
     html = open("https://flatironschool.com/")
     doc = Nokogiri::HTML(html)
-    course_offerings=doc.css("#2a778efd-1685-5ec6-9e5a-0843d6a88b7b .inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
+    course_offerings=doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
     puts course_offerings
   end
   def make_courses
